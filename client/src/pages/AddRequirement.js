d46db414598fbe167407da9c4e9ce5e6895635dc
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2020-09-03 17:20:02 +0300

    feat: change mupltiple inputs on change

--- client/src/pages/AddRequirement.js
+++ client/src/pages/AddRequirement.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './AddRequirement.scss';
 import '../index.scss';
 const AddRequirement = () => {
-  const [requirement, setRequirement] = useState({ title: 'title', description: '', minScore: 0, maxScore: 0, onlyForMentors: true,  scopeType: ''});
+  const [requirement, setRequirement] = useState({ title: '', description: '', minScore: 0, maxScore: 0, onlyForMentors: false,  scopeType: ''});
   const [requirements, setRequirements] = useState({basicScope: [], advancedScope: [], extraScope: []});
 
   const changeRequirement = event => {
@@ -15,6 +15,14 @@ const AddRequirement = () => {
       <h1>Create new requirement</h1>
       <label htmlFor="title">Title: </label>
       <input id="title" name="title" onChange={changeRequirement}/>
+      <label htmlFor="description">Description: </label>
+      <textarea id="description" name="description" onChange={changeRequirement}/>
+      <label htmlFor="minScore">Minimum Score: </label>
+      <input id="minScore" name="minScore" onChange={changeRequirement}/>
+      <label htmlFor="maxScore">Maximum Score: </label>
+      <input id="maxScore" name="maxScore" onChange={changeRequirement}/>
+      <label htmlFor="onlyForMentors">Only for mentors: </label>
+      <input type="checkbox" id="onlyForMentors" name="onlyForMentors" onChange={changeRequirement}/>
       <button>Add requirement</button>
     </div>
   );
