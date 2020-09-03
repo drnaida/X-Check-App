594fe70a35978df940739130ed5b95fa41f3c0cc
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2020-09-03 15:53:20 +0300

    feat: change state on form input change

--- client/src/pages/AddRequirement.js
+++ client/src/pages/AddRequirement.js
@@ -2,12 +2,19 @@ import React, { useState } from 'react';
 import './AddRequirement.scss';
 import '../index.scss';
 const AddRequirement = () => {
-  const [requirement, setRequirement] = useState({ title: '', description: '', minScore: 0, maxScore: 0, onlyForMentors: true,  scopeType: ''});
+  const [requirement, setRequirement] = useState({ title: 'title', description: '', minScore: 0, maxScore: 0, onlyForMentors: true,  scopeType: ''});
   const [requirements, setRequirements] = useState({basicScope: [], advancedScope: [], extraScope: []});
+
+  const changeRequirement = event => {
+    console.log(requirement);
+    setRequirement({ ...requirement, [event.target.name]: event.target.value });
+  };
 
   return(
     <div className="AddRequirement">
       <h1>Create new requirement</h1>
+      <label htmlFor="title">Title: </label>
+      <input id="title" name="title" onChange={changeRequirement}/>
       <button>Add requirement</button>
     </div>
   );
