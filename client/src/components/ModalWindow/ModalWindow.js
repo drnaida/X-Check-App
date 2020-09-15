7a618f47a9d1a692ed1597f5f794e4f4e9c7b06a
Aliaksandr Torkhau <torkhau@gmail.com>
2020-09-15 12:38:09 +0300

    feat: ModalWindow have props 'buttons' - array of buttons

--- client/src/components/ModalWindow/ModalWindow.js
+++ client/src/components/ModalWindow/ModalWindow.js
@@ -4,7 +4,32 @@ import { Modal } from 'antd';
 import './ModalWindow.scss';
 
 export const ModalWindow = props => {
-  const { title, visible, handlerOkButton, handlerCancelButton, children, height = '90vh' } = props;
+  const {
+    title,
+    visible,
+    handlerOkButton,
+    handlerCancelButton,
+    children,
+    height = '90vh',
+    buttons = []
+  } = props;
+  if (buttons.length > 0) {
+    return (
+      <Modal
+        title={title}
+        visible={visible}
+        centered
+        width="90%"
+        style={{ height }}
+        okText="Save"
+        onOk={handlerOkButton}
+        onCancel={handlerCancelButton}
+        footer={buttons}
+      >
+        {children}
+      </Modal>
+    );
+  }
   return (
     <Modal
       title={title}
